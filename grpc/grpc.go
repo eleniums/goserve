957f54542051e19c03cc84d57d9aaceb62990f2b
eleniums <eleniums@yahoo.com>
2019-01-25 19:22:01 -0800

    Serve grpc with max send/receive limits

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,20 +1,52 @@
 package grpc
 
 import (
+	"crypto/tls"
+	"math"
 	"net"
 
 	"google.golang.org/grpc"
 )
 
 type GRPC struct {
+	// Server is used to register server handlers.
 	Server *grpc.Server
+
+	// TLSConfig stores the TLS configuration if a secure endpoint is desired.
+	TLSConfig *tls.Config
+
+	// Opt is an array of server options for customizing the server further.
+	Opt []grpc.ServerOption
+
+	// UnaryInterceptors is an array of unary interceptors. They will be executed in order, from first to last.
+	UnaryInterceptors []grpc.UnaryServerInterceptor
+
+	// StreamInterceptors is an array of stream interceptors. They will be executed in order, from first to last.
+	StreamInterceptors []grpc.StreamServerInterceptor
+
+	// MaxSendMsgSize will change the size of the message that can be sent from the service.
+	MaxSendMsgSize int
+
+	// MaxRecvMsgSize will change the size of the message that can be received by the service.
+	MaxRecvMsgSize int
 }
 
 func New() *GRPC {
-	return &GRPC{}
+	return &GRPC{
+		MaxSendMsgSize: math.MaxInt32,
+		MaxRecvMsgSize: math.MaxInt32,
+	}
 }
 
 func (g *GRPC) Serve(l net.Listener) error {
-	// TODO: implement serve
-	return nil
+	return g.Server.Serve(l)
+}
+
+func (g *GRPC) Initialize() {
+	opt := []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(g.MaxRecvMsgSize),
+		grpc.MaxSendMsgSize(g.MaxSendMsgSize),
+	}
+
+	g.Server = grpc.NewServer(opt...)
 }
