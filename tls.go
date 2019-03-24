6079202e352fc94a8ea9474656bae3ab686ec615
eleniums <eleniums@yahoo.com>
2019-03-24 15:51:19 -0700

    Add method for loading TLS configuration

--- tls.go
+++ tls.go
@@ -1,3 +1,31 @@
 package goserve
 
-// TODO: load tlsConfig here
+import (
+	"crypto/tls"
+)
+
+// TLS will load a cert/key pair and return a TLS config.
+func TLS(certFile, keyFile string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+		Certificates: []tls.Certificate{cert},
+	}, nil
+}
